aff3ffa32b8fc5ae7f2ca566e48b13f9c46eb82d
Neto <netoodeveloper@gmail.com>
2024-02-20 07:55:21 -0300

    Calculando resultado

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -42,7 +42,7 @@ export class QuizzComponent implements OnInit{
 
   playerChoice(value:string){
     this.answers.push(value);
-    console.log(this.answers);
+    this.nextStep();
   }
 
   async nextStep(){
@@ -51,7 +51,26 @@ export class QuizzComponent implements OnInit{
     if (this.questionMaxIndex > this.questionIndex) {
       this.questionSelected = this.questions[this.questionIndex];
     } else {
+      const finalAnswer:string = await this.checkResult(this.answers);
+
       this.finished = true;
+      this.answerSelected = quizz_questions.results[finalAnswer as keyof typeof quizz_questions.results]
+      //Verificar opção verdadeira
     }
   }
+
+  async checkResult(answers:string[]){
+
+    const result = answers.reduce((previous, current, i, arr) => {
+      if (arr.filter(item => item === previous).length > arr.filter(item => item === current).length) {
+        return previous;
+      } else {
+        return current;
+      }
+    })
+
+    return result;
+  }
+
+
 }
